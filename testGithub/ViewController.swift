4284761f6381ab61d7ab50d3cdc8e9ee48e7c62b
Waad Alkatheri <waadalkatheri@waads-macbook-pro.local>
2019-02-10 20:08:24 +0300

    Initial Commit

---
+++ testGithub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testGithub
+//
+//  Created by Waad Alkatheri on 05/06/1440 AH.
+//  Copyright © 1440 Waad Alkatheri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
