26cb5a856bcaf5f2fde25fab0aa29756a8db6025
Waad Alkatheri <waadalkatheri@waads-macbook-pro.local>
2019-02-10 20:11:58 +0300

    test

--- testGithub/ViewController.swift
+++ testGithub/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var testvar = "hello Gethub"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
