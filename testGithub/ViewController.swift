2741ad95dedd5d1d39422d8bab6e0b8cd4fe09fe
Waad Alkatheri <waadalkatheri@waads-macbook-pro.local>
2019-02-10 20:16:26 +0300

    tes3

--- testGithub/ViewController.swift
+++ testGithub/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
+    var test = 34 
     var testvar = "hello Gethub"
     override func viewDidLoad() {
         super.viewDidLoad()
